80efebb32410597cdb9b7d5d48bb2a3d66395d24
kdhg696 <kdhg696@astrazeneca.net>
2023-05-08 15:07:16 -0600

    Finished connecting services

--- src/app/dashboard/pages/alumnos/components/alumno-details/alumno-details.component.ts
+++ src/app/dashboard/pages/alumnos/components/alumno-details/alumno-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { AlumnosService } from '../../services/alumnos.service';
 import { InscripcionesService } from '../../../inscripciones/services/inscripciones.service';
+import { Inscripcion } from '../../../inscripciones/models';
 
 @Component({
   selector: 'app-alumno-details',
@@ -19,13 +20,14 @@ export class AlumnoDetailsComponent implements OnInit{
    this.route.params.subscribe((params: Params) => {
     this.alumnosService.getAlumnoById(params['id']).subscribe((alumno) => this.alumno = alumno);
     this.inscripcionesService.getInscripcionByAlumnoId(params['id']).subscribe((response) => this.inscripciones = response);
-    this.icon = this.alumno.sexo === "Femenino" ? "assets/img/female-icon.jpg" : "assets/img/male-icon.jpg";
   })
  }
 
  id = 0;
- icon = ""
  alumno = {id: 1, nombre: "Guadalupe", apellido: "Osuna", sexo: "Femenino", email: "lupita@email.com", pais: "Mexico"}
  inscripciones = [{id: 0, alumno: {id: 0, nombre: "", apellido: "", sexo: "", email: "", pais: ""}, curso: {id: 0, nombre: "", categoria: "", fechaInicio: new Date(), fechaFin: new Date(), info: ""}}]
-
+ icon = this.alumno.sexo === "Femenino" ? "assets/img/female-icon.jpg" : "assets/img/male-icon.jpg";
+ eliminarInscripcion(inscripcion: Inscripcion): void {
+  this.inscripcionesService.deleteInscripcion(inscripcion.id);
+ }
 }
